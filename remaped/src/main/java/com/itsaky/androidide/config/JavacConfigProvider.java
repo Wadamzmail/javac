9737aa55d0cd0e132a0bcf1c905b6f6075a25acf
Wadamzmail <alksndrmetwakil@gmail.com>
2026-05-08 19:45:22 +0200

    fix

--- remaped/src/main/java/com/itsaky/androidide/config/JavacConfigProvider.java
+++ remaped/src/main/java/com/itsaky/androidide/config/JavacConfigProvider.java
@@ -15,7 +15,7 @@
  *   along with AndroidIDE.  If not, see <https://www.gnu.org/licenses/>.
  */
 
-package com.itsaky.androidide.javac.config;
+package com.itsaky.androidide.config;
 
 import jdkx.lang.model.SourceVersion;
 import java.lang.reflect.Field;
